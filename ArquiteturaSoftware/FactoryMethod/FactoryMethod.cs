f51eed8a94b63f9853eae00b0376fe6133ffb834
Anderson de Oliveira Lima <andersonlima_guitar@hotmail.com>
2023-02-05 13:38:54 -0300

    Inicio Factory Method

---
+++ ArquiteturaSoftware/FactoryMethod/FactoryMethod.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquiteturaSoftware.FactoryMethod
+{
+    internal class FactoryMethod
+    {
+    }
+
+    abstract class FormaGeometrica
+    {
+        public abstract double GetArea();
+        public abstract double GetPerimetro();
+    }
+
+    class Retangulo : FormaGeometrica
+    {
+        public override double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetPerimetro()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Circulo : FormaGeometrica
+    {
+        public override double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetPerimetro()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Triangulo : FormaGeometrica
+    {
+        public override double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetPerimetro()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
